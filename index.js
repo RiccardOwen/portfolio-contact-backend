2f073f5d54ec6f9797e06c4595e6e8c5712481db
Danny Arellano <139287708+RiccardOwen@users.noreply.github.com>
2025-06-10 01:59:58 -0600

    Commit cors edit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://riccardowen.github.io'
+}));
 app.use(express.json());
 
 // Contact form endpoint
